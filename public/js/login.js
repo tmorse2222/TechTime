ad9d71062f8773e4ec6c92a0e28a6637835e9020
Tyler M <tmorse2222@gmail.com>
2022-11-06 09:51:58 -0700

    Added functionality to allow user to login

--- public/js/login.js
+++ public/js/login.js
@@ -17,4 +17,25 @@ const signupFormHandler = async (event) => {
     }
 }
 
+// Functionality for login form
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+    const username = document.querySelector('#username-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+    if (username && password) {
+        const response = await fetch('/api/users/login', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+
 document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
