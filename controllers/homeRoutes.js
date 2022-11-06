3934a8a061599cedd9b3d7a37106e92eaae29452
Tyler M <tmorse2222@gmail.com>
2022-11-06 16:25:17 -0700

    Bug fix to allow '/homepage' to render the same as '/'

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -80,8 +80,24 @@ router.get('/dashboard', withAuth, async (req, res) => {
     }
 });
 
-router.get('/homepage', (req, res) => {
-    res.render('homepage');
+router.get('/homepage', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
+        const posts = postData.map((post) => post.get({ plain: true }));
+        res.render('homepage', {
+            posts,
+            logged_in: req.session.logged_in,
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
