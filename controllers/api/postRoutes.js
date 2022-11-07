b1df498fa8723d9f1bc3361a0899f340b809d9ba
Tyler M <tmorse2222@gmail.com>
2022-11-06 17:28:09 -0700

    Added 'View Post' button to allow individual viewing of post(No handlebars yet)

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -15,4 +15,33 @@ router.post('/', withAuth, async (req, res) => {
     }
 });
 
+// GET post by id
+router.get('/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+                {
+                    model: Comment,
+                    attributes: ['id', 'comment_text', 'post_id', 'user_id'],
+                    include: {
+                        model: User,
+                        attributes: ['username'],
+                    },
+                },
+            ],
+        });
+        if (!postData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
---
+++ public/js/homepage.js
@@ -0,0 +1,17 @@
+// Function for viewing a single message
+const viewMessageHandler = async (event) => {
+    event.preventDefault();
+    const id = event.target.getAttribute('id');
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'GET',
+    });
+    if (response.ok) {
+        document.location.replace(`/post/${id}`);
+    } else {
+        alert(response.statusText);
+    }
+}
+
+document.querySelectorAll('.view-post-btn').forEach((button) => {
+    button.addEventListener('click', viewMessageHandler);
+});
