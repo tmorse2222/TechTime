3060e9b74476ec68a44af14c1b78b538e50f1e58
Tyler M <tmorse2222@gmail.com>
2022-11-07 21:27:32 -0700

    Created functionality for logout button; functionality of application complete

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -91,4 +91,16 @@ router.delete('/posts/:id', withAuth, async (req, res) => {
     }
 });
 
+// POST logout
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+
 module.exports = router;
